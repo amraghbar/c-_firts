c291b785e773ff4824fb96a56580931b2ce23310
amraghbar <amrtaghbar@gmail.com>
2024-08-04 01:50:58 +0300

    C#_first

---
+++ task/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task.Models;
+
+namespace task.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=task_Product;Trusted_Connection=true;TrustServerCertificate=True");
+        }
+    }
+}
